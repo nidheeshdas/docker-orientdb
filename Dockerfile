c98bd7a73a7756472481155f04ce2d0f93a2347c
nidheeshdas thavorath <nidheeshdas@thavorath.com>
2016-03-16 15:53:32 +0530

    wip: updates

--- Dockerfile
+++ Dockerfile
@@ -9,11 +9,14 @@ ADD init /opt/init
 # giddyup
 ADD giddyup /bin
 
+# RUN
+RUN curl -o orientdb.tar.gz "http://orientdb.com/download.php?file=orientdb-community-2.1.13.tar.gz&os=linux" && mkdir -p /opt/orient && tar -zxvf orientdb.tar.gz --strip-components=1 --directory /opt/orient && rm -rf orientdb.tar.gz && rm -rf /opt/orient/config && mv /opt/conf /opt/orient/config && rm -rf /opt/orient/databases/*
+
 # Expose the necessary ports
 EXPOSE 2424 2480 5701
 
 # Setup default environment
-ENV IP=127.0.0.1 MEM=512M TCP=true AWS=false AWS_ACCESS_KEY=NOTSET AWS_SECRET_KEY=NOTSET AWS_REGION=eu-west-1 AWS_SECURITY_GROUP=default VERSION=2.1.12
+ENV IP=127.0.0.1 MEM=512M TCP=true AWS=false AWS_ACCESS_KEY=NOTSET AWS_SECRET_KEY=NOTSET AWS_REGION=eu-west-1 AWS_SECURITY_GROUP=default VERSION=2.1.13
 
 # Set the default command
 CMD /opt/init/orient.sh
--- init/orient.sh
+++ init/orient.sh
@@ -5,7 +5,7 @@ export ORIENTDB_HOME='/opt/orient'
 export ORIENTDB_VERSION=${VERSION}
 export ORIENTDB_NODE_NAME=$(hostname)
 
-curl -o orientdb.tar.gz http://orientdb.com/download.php?file=orientdb-community-${VERSION}.tar.gz&os=linux && mkdir -p /opt/orient && tar -zxvf orientdb.tar.gz --strip-components=1 --directory /opt/orient && rm -rf orientdb.tar.gz && rm -rf /opt/orient/config && mv /opt/conf /opt/orient/config && rm -rf /opt/orient/databases/*
+# curl -o orientdb.tar.gz "http://orientdb.com/download.php?file=orientdb-community-${VERSION}.tar.gz&os=linux" && mkdir -p /opt/orient && tar -zxvf orientdb.tar.gz --strip-components=1 --directory /opt/orient && rm -rf orientdb.tar.gz && rm -rf /opt/orient/config && mv /opt/conf /opt/orient/config && rm -rf /opt/orient/databases/*
 
 # java -Xmx${MEM} -Dhazelcast.ip=${IP} -Dhazelcast.tcp=false -Dhazelcast.aws=true -Dhazelcast.access=${AWS_ACCESS_KEY} -Dhazelcast.secret=${AWS_SECRET_KEY} -Dhazelcast.region=${AWS_REGION} -Dhazelcast.group=${AWS_SECURITY_GROUP} -Djna.nosys=true -XX:+HeapDumpOnOutOfMemoryError -Djava.awt.headless=true -Dfile.encoding=UTF8 -Drhino.opt.level=9 -Ddistributed=true -Djava.util.logging.config.file="${ORIENTDB_HOME}/config/orientdb.properties" -Dorientdb.config.file="${ORIENTDB_HOME}/config/orientdb.xml" -Dorientdb.www.path="${ORIENTDB_HOME}/www" -cp "${ORIENTDB_HOME}/lib/orientdb-server-${ORIENTDB_VERSION}.jar:${ORIENTDB_HOME}/lib/*" $* com.orientechnologies.orient.server.OServerMain
 
